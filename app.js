87b519a44b5f800d330d6631e26cbb7385c74467
Letícia <leticia.souza@naxi.com.br>
2022-07-12 17:14:52 -0300

    finalized

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 new Vue({
     el:'#app',
     data:{
-        playerLife: 0,
-        monsterLife: 30
+        running: false,
+        playerLife: 100,
+        monsterLife: 100,
+        logs:[]
     },
     computed:{
         hasResult(){
@@ -10,9 +12,45 @@ new Vue({
         }
     },
     methods:{
-
+        startGame(){
+            this.running = true
+            this.playerLife = 100
+            this.monsterLife = 100
+            this.logs = []
+        },
+        getRandom(min, max){
+            const value = Math.random() * (max - min) + min  //multiplico o valor randomico com o resultado do max e min.
+            return Math.round(value) // arendodo o valor randomico.
+        },
+        attack(especial){
+            this.hurt('playerLife', 7, 12, false, 'Monster', 'Player', 'monster') 
+            //'Monster' = quem originou o evento. 'Player' = quem recebeu o evento. 'player' = classe CSS
+            if(this.monsterLife > 0)
+                this.hurt('monsterLife', 5, 10, especial, 'Player', 'Monster', 'player') // monstro ta sujeito a um ataque especial.
+        },
+        hurt(atr,min, max, especial, source, target, clas){ // atr é o nome que está em this.hurt
+            const plus = especial ? 5 : 0  //ao clicar em special attack é adicionado um plus.
+            const hurt = this.getRandom(min + plus, max + plus)
+            this[atr]= Math.max(this[atr] - hurt, 0) // utilizando o math.max para evitar que fique negativo
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`, clas)
+        },
+        heal(min, max){
+            const heal = this.getRandom(min,max) 
+            this.playerLife = Math.min(this.playerLife + heal, 100) // usando o min para não ter o acrescimo de vida maior que 100.
+            this.registerLog(`Jogador ganhou força de ${heal}.`, 'player')
+        },
+        healAndHurt(){ //Depois que o player se cura, ele pode receber ataque.
+            this.heal(10, 15)
+            this.hurt('playerLife', 7, 12, false, 'Monster', 'Player', 'monster') //Apenas o player pode ser ferido com ataque especial, por isso, false.
+        },
+        registerLog(text, clas){
+            this.logs.unshift({text, clas})
+        }
     },
     watch:{
-
+        hasResult(value){
+            if(value) this.running = false
+        }
     }
 })
+
--- readme.md
+++ readme.md
@@ -4,11 +4,12 @@ Jogo criado para exercitar o que foi aprendido até o momento, no curso de VueJS
 
 ## Instruções do jogo:
 
-* Attack: usado um ataque mais fraco do que o mostro, tendo grandes possibilidades de perder mais vida do que tirar;
-* Special Attack: é um ataque especial e tem mais chances de retirar mais vida do que perder;
-* Health: Usado para recuperar um pouco de vida, porém, enquanto você clica para recuperar sua saúde o monstro pode atacar normalmente;
-* Quit: Sair do jogo, e ir para tela inicia.
+* **Attack**: usado um ataque mais fraco do que o mostro, tendo grandes possibilidades de perder mais vida do que tirar;
+* **Special Attack**: é um ataque especial e tem mais chances de retirar mais vida do que perder;
+* **Heal**: Usado para recuperar um pouco de vida, porém, enquanto você clica para recuperar sua saúde o monstro pode atacar normalmente;
+* **Quit**: Sair do jogo, e ir para tela inicia.
 
 É impresso na tela os logs dos ataques feitos e recebidos, e o quanto de vida você tirou do monstro e quanto ele tirou sua.
+Os logs mais recentes ficam por primeiro.
 
 Nessa aplicação, foi utilizado CSS puro e VueJS, exercitando as diretivas como v-if, v-else, v-for, v-show, @click e afins. Exercitando também methods, computed e watch.
